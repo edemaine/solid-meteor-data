fe5d6d2880859482b1908e3ba16d96086b3b37c3
Erik Demaine <edemaine@mit.edu>
2021-12-30 15:54:32 -0500

    Draft of createTracker

---
+++ createTracker.ts
@@ -0,0 +1,20 @@
+import {Tracker} from 'meteor/tracker';
+import {createComputed, createSignal, onCleanup} from 'solid-js';
+import type {Accessor} from 'solid-js';
+
+// Types from react-meteor-data
+export interface IReactiveFn<T> {
+  (c?: Tracker.Computation): T
+}
+
+export const createTracker = <T = any>(reactiveFn: IReactiveFn<T>): Accessor<T> => {
+  const [output, setOutput] = createSignal<T>();
+  let computation: Tracker.Computation;
+  createComputed(() => {
+    if (computation) computation.stop();
+    computation = Tracker.autorun((c) =>
+      setOutput(() => reactiveFn(c)))
+  });
+  onCleanup(computation!.stop);
+  return output as Accessor<T>; // initial undefined should be overwritten now
+}
