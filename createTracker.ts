5a364421c376b44b3dc85db0bf1d8082fea5f77e
Erik Demaine <edemaine@mit.edu>
2022-07-12 15:47:53 -0400

    Rewrite createTracker as createMemo (fix #8)
    This rewrite both is simpler and better respects Solid's reactive graph,
    which encourages use of `createMemo` over `createTracker`.

--- createTracker.ts
+++ createTracker.ts
@@ -1,5 +1,5 @@
 import {Tracker} from 'meteor/tracker';
-import {createComputed, createMemo, createSignal, onCleanup} from 'solid-js';
+import {createMemo, createSignal, onCleanup} from 'solid-js';
 import type {Accessor} from 'solid-js';
 
 // Type from react-meteor-data
@@ -14,30 +14,24 @@ export const mode = {
 export const createTracker = <T>(reactiveFn: IReactiveFn<T>): Accessor<T> => {
   if (mode.auto)
     return createMemo(() => reactiveFn());
-  const [output, setOutput] = createSignal<T>();
   const [resettable, reset] = createSignal(undefined, {equals: false});
-  let computation: Tracker.Computation;
-  createComputed(() => {
+  return createMemo(() => {
     resettable(); // depend on reset signal
-    if (computation) computation.stop();
+    let output: T;
     // Wrap this Tracker creation in nonreactive so it doesn't accidentally
     // get a parent Tracker (e.g. if SolidJS signal gets set within tracker).
-    computation = Tracker.nonreactive(() => Tracker.autorun((c) => {
-      // Only run the function in first Tracker run, because only that is run
-      // in the correct Listener context of createComputed.  Future runs are
-      // executed within a setImmediate context, so just reset in that case.
+    const computation = Tracker.nonreactive(() => Tracker.autorun((c) => {
+      // Only run the function in first Tracker run, because only that run is
+      // synchronous and thus in the correct SolidJS Listener context from
+      // `createMemo`.  Future runs are executed within a `setImmediate`
+      // context, so just reset in that case.
       if (c.firstRun) {
-        // Only the call to reactiveFn should be done in Tracker; all SolidJS
-        // signal setting needs to be explicitly not tracked because it can
-        // end up immediately recalling this createComputed() function, which
-        // would nest the new Tracker within the old one which gets stopped.
-        const result = reactiveFn(c);
-        Tracker.nonreactive(() => setOutput(() => result));
+        output = reactiveFn(c);
       } else {
         Tracker.nonreactive(reset);
       }
     }));
+    onCleanup(() => computation.stop());
+    return output!; // initial undefined should be overwritten by firstRun
   });
-  onCleanup(() => computation!.stop());
-  return output as Accessor<T>; // initial undefined should be overwritten now
 };
